a27f905acece486077d3cfc9a33c1dd125e1e488
Rathan-r-nayak <rathannayak300@gmail.com>
2023-06-22 15:49:10 +0530

    added backend to login forms

--- LibraryManager/loginApp/urls.py
+++ LibraryManager/loginApp/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 
 urlpatterns=[
-    path("",views.welcomePage),
-    path("signin/",views.signinPage),
-    path("signup/",views.signupPage),
+    path("",views.welcomePage,name="main"),
+    # path("signin/",views.signinPage,name="singinPage"),
+    path("signinInput",views.signinInput,name="singinInput"),
+
+
+    # path("signup/",views.signupPage,name="signupPage"),
+    path("signupInput",views.signupInput,name="singupInput"),
     path("filldb/",views.filldb)
 ]
--- LibraryManager/loginApp/views.py
+++ LibraryManager/loginApp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 from django.template import loader
-from django.http import HttpResponse
 from . import models
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -9,15 +12,72 @@ def welcomePage(request):
     template=loader.get_template("loginApp/index.html")
     return render(request,"loginApp/index.html")
 
+
+
+
 def signinPage(request):
     template=loader.get_template("loginApp/signinPage.html")
     # return HttpResponse(template.render())
     return render(request,"loginApp/signinPage.html")
 
+def signinInput(request):
+    if request.method=='POST':
+        uname=request.POST['uname']
+        upasswd=request.POST['passwd']
+
+        user=authenticate(username=uname,password=upasswd)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,f"successfully logged in as {user}")
+            return redirect('main')
+        else:
+            messages.error(request,f"failed to login")
+            return redirect('main')
+    
+    return HttpResponse("404-Not Found")
+
+
+
+
+
+
+
 def signupPage(request):
     template=loader.get_template("loginApp/signupPage.html")
     # return HttpResponse(template.render())
     return render(request,"loginApp/signupPage.html")
+
+def signupInput(request):
+    if request.method=='POST':
+        uname=request.POST['uname']
+        umail=request.POST['email']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        cardnum=request.POST['card_num']
+        upasswd=request.POST['passwd']
+
+        ob=User.objects.create_user(username=uname,password=upasswd)
+        ob.first_name=fname
+        ob.last_name=lname
+        ob.email=umail
+        ob.save()
+        
+        u=models.AuthUser.objects.get(username=uname)
+        user=models.User(uid=u,card_num=cardnum,total_issued=0)
+        user.save()
+        login(request,u)
+        return redirect('main')
+    return HttpResponse("404-Not Found")
+
+
+
+
+
+
+
+
+
 
 
 
